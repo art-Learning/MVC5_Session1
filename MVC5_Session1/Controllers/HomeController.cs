919074d3b3a88f20892eca3087e12b845e40fcaa
art <partypeopleland@gmail.com>
2016-08-07 00:47:14 +0800

    實作Services層

--- MVC5_Session1/Controllers/HomeController.cs
+++ MVC5_Session1/Controllers/HomeController.cs
@@ -9,8 +9,12 @@ namespace MVC5_Session1.Controllers
 {
     public class HomeController : Controller
     {
-        private MoneyEntity db = new MoneyEntity();
+        private readonly AcctBookSvc _acctBookSvc;
 
+        public HomeController()
+        {
+            _acctBookSvc = new AcctBookSvc();
+        }
         public ActionResult Index()
         {
             return View();
@@ -32,49 +36,8 @@ namespace MVC5_Session1.Controllers
         [ChildActionOnly]
         public ActionResult MoneyList()
         {
-            List<MoneyRecordViewModel> model = getFakeData();
-            //List<MoneyRecordViewModel> model = getRealData();
+            var model = _acctBookSvc.getRealData();
             return View(model);
-        }
-
-        private List<MoneyRecordViewModel> getRealData()
-        {
-            List<MoneyRecordViewModel> model = db.AccountBook
-                .Take(5)
-                .Select(a => new MoneyRecordViewModel
-                {
-                    amount = a.Amounttt,
-                    createDts = a.Dateee,
-                    moneyType = (MoneyType)(a.Categoryyy+1)  //Enum:1,2 Db:0,1
-                }).ToList();
-            return model;
-        }
-        /// <summary>
-        /// 開發時期View所使用的假資料
-        /// </summary>
-        /// <returns></returns>
-        private static List<MoneyRecordViewModel> getFakeData()
-        {
-            var model = new List<MoneyRecordViewModel>();
-            model.Add(new MoneyRecordViewModel()
-            {
-                amount = 300,
-                moneyType = MoneyType.Pay,
-                createDts = DateTime.Now
-            });
-            model.Add(new MoneyRecordViewModel()
-            {
-                amount = 1600,
-                moneyType = MoneyType.Pay,
-                createDts = DateTime.Now.AddDays(1)
-            });
-            model.Add(new MoneyRecordViewModel()
-            {
-                amount = 800,
-                moneyType = MoneyType.Pay,
-                createDts = DateTime.Now.AddDays(2)
-            });
-            return model;
         }
     }
 }
---
+++ MVC5_Session1/Models/Services/AcctBookSvc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MVC5_Session1.Models
+{
+    public class AcctBookSvc
+    {
+        private MoneyEntity _db;
+        public AcctBookSvc()
+        {
+            _db = new MoneyEntity();
+        }
+
+        public List<MoneyRecordViewModel> getRealData()
+        {
+            List<MoneyRecordViewModel> model = _db.AccountBook
+                .Take(5)
+                .Select(a => new MoneyRecordViewModel
+                {
+                    amount = a.Amounttt,
+                    createDts = a.Dateee,
+                    moneyType = (MoneyType)(a.Categoryyy + 1)  //Enum:1,2 Db:0,1
+                }).ToList();
+            return model;
+        }
+    }
+}
