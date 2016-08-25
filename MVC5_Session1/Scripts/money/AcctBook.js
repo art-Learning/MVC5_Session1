88380a62cbc7fde96afe2625cdf1947ee00a3513
art <art@cowell.com.tw>
2016-08-25 13:02:10 +0800

    加入驗證的HTML 於頁面中引用js 改用低調的js寫法

---
+++ MVC5_Session1/Scripts/money/AcctBook.js
@@ -0,0 +1,6 @@
+﻿function cleanUpFrm() {
+    $("input[name='money']").val('');
+    $("textarea[name='description']").val('');
+    $("input[name='date']").val('');
+    $("select[name='category']").prop('selectedIndex', 0);
+}
