f5698353b95deaf177ed805df6ad453603c92a2c
art <art@cowell.com.tw>
2016-08-25 13:13:31 +0800

    RULE01:所有欄位必填 - ok RULE02:金額，僅允許正整數 - ok RULE04:備註，最多100字元 - ok

--- MVC5_Session1/Models/ViewModel/MoneyFormViewModel.cs
+++ MVC5_Session1/Models/ViewModel/MoneyFormViewModel.cs
@@ -11,25 +11,36 @@ namespace MVC5_Session1.Models.ViewModel
     /// </summary>
     public class MoneyFormViewModel
     {
+        //RULE01:所有欄位必填
+        //RULE02:金額，僅允許正整數
+        //RULE03:日期，不可大於今天 (允許小於等於)
+        //RULE04:備註，最多100字元
+
         /// <summary>
         /// 類別
         /// </summary>
         [Display(Name ="類別")]
+        [Required(ErrorMessage ="請選擇{0}")]
         public int category { get; set; }
         /// <summary>
         /// 日期
         /// </summary>
         [Display(Name ="日期")]
+        [Required(ErrorMessage ="請輸入{0}")]
         public DateTime date { get; set; }
         /// <summary>
         /// 金額
         /// </summary>
         [Display(Name ="金額")]
+        [Required(ErrorMessage ="請輸入{0}")]
+        [Range(1,int.MaxValue,ErrorMessage ="{0}僅允許輸入正整數")]
         public int money { get; set; }
         /// <summary>
         /// 備註
         /// </summary>
         [Display(Name ="備註")]
+        [Required(ErrorMessage ="請輸入{0}")]
+        [StringLength(100,MinimumLength =0,ErrorMessage ="{0}最多只可以輸入100字元")]
         public string description { get; set; }
     }
 }
--- MVC5_Session1/Properties/AssemblyInfo.cs
+++ MVC5_Session1/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // 您可以指定所有值或預設修訂和組件數目
 // 指定為預設值:
-[assembly: AssemblyVersion("2016.8.25.0458")]
-[assembly: AssemblyFileVersion("2016.8.25.0458")]
+[assembly: AssemblyVersion("2016.8.25.0511")]
+[assembly: AssemblyFileVersion("2016.8.25.0511")]
