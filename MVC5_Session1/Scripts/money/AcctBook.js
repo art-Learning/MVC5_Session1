31408d54f16964d47d0ff464c1367a1f05357178
art <art@cowell.com.tw>
2016-08-26 00:57:08 +0800

    整理Script到同一個檔案

--- MVC5_Session1/Scripts/money/AcctBook.js
+++ MVC5_Session1/Scripts/money/AcctBook.js
@@ -4,3 +4,20 @@
     $("input[name='date']").val('');
     $("select[name='category']").prop('selectedIndex', 0);
 }
+$.validator.addMethod("aftertoday", function (value, element, param) {
+    if (value == null) {
+        return true;
+    }
+    var sysDt = new Date();
+    var usrDt = new Date(value);
+    var curDt = new Date(sysDt.getFullYear(), sysDt.getMonth(), sysDt.getDate());
+    usrDt.setHours(0);
+    var sysNum = Date.parse(curDt);
+    var usrNum = Date.parse(usrDt);
+    if (usrNum > sysNum) {
+        return false;
+    } else {
+        return true;
+    }
+});
+$.validator.unobtrusive.adapters.addSingleVal("aftertoday", "input");
